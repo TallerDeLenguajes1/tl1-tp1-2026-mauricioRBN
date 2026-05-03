930bd7f5c159ca1021e3f0a3cf36542d72d81480
Mauricio <mauriciofernandez2108@gmail.com>
2026-05-03 01:15:27 -0300

    segunda parte punto 4

--- tp1_1.c
+++ tp1_1.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
 int main(){
     printf("Hola Mundo");
+    int a=10;
+    int *p=&a;
+    printf("El contenido del puntero: %d \n", *p);
+    printf("La direccion de memoria almacenada en el puntero es: %p \n",p);
+    printf("La direccion de memoria de la variable apuntada es: %p \n",&a);
+    printf("La direccion de memoria del puntero es: %p \n",&p);
+    printf("El tamaño de momoria de la variable original: %d bytes\n",sizeof(a));
+
 }
